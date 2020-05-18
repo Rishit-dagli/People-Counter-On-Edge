e4f93f9c1d6e75a823a6e0d5030fb219562034d6
Rishit Dagli <39672672+Rishit-dagli@users.noreply.github.com>
2020-05-18 12:56:43 +0530

    Styled comments

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 """People Counter."""
 
 '''
-python main.py -i resources/Pedestrian_Detect_2_1_1.mp4 -m person-detection-retail-0013/FP32/person-detection-retail-0013.xml -l /opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so -d CPU -pt 0.6 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://0.0.0.0:3004/fac.ffm
+To run the model-
+
+python main.py -i resources/Pedestrian_Detect_2_1_1.mp4 -m person-detection-retail-0013/FP32/person-detection-retail-0013.xml -l 
+/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so -d CPU -pt 0.6 | ffmpeg -v 
+warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://0.0.0.0:3004/fac.ffm
 '''
+
 import os
 import sys
 import time
